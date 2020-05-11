6a9f3e2612402d3bce89dc36697dd26bbb8168f7
Himanshu Singh <himanshu.singh@nagarrro.com>
2020-05-11 21:05:48 +0530

    docker image v1

--- user/src/app.js
+++ user/src/app.js
@@ -4,8 +4,12 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get("/", ( req, res ) => {
-	res.json({msg: "Hello World"});
+app.get("/", (req, res ) => {
+	res.json({msg: "Hello World new"});
+});
+
+app.get("/user/:id", (req, res) => {
+	res.json({ req: req.params.id });
 });
 
 module.exports = app;
