b3375b81b28f9aa2870b61491822bbcd18d193f8
Himanshu Singh <himanshu.singh@nagarrro.com>
2020-05-13 16:24:25 +0530

    added mongodb support and docker-compose

--- user/src/app.js
+++ user/src/app.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res ) => {
-	res.json({msg: "Hello World new"});
+
+// Connect to MongoDB
+mongoose
+    .connect(
+        'mongodb://mongodb:27017/test',
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+    res.json({ msg: "hello from user microservice" });
+});
+
+app.get("/users", (req, res ) => {
+    User.find()
+        .then(users => res.json({ allUsers: users }))
+        .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
 
 app.get("/user/:id", (req, res) => {
 	res.json({ req: req.params.id });
+});
+
+app.post('/user', (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email
+    });
+
+    newUser.save().then(user => res.json({ addedUser: user }));
 });
 
 module.exports = app;
